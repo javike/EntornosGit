7fc47dc53a5f40f859b9be20be6037817b6ee626
alumno <alumno@alumno-1dam>
2021-05-25 10:24:31 +0200

    first

---
+++ javike/src/javike/Vehiculo.java
@@ -0,0 +1,7 @@
+package javike;
+
+public class Vehiculo (int numPuertas, int numRuedas){
+ this.numPuertas = numPuertas;
+ this.numRuedas = numRuedas;
+}
+}
---
+++ javike/src/module-info.java
@@ -0,0 +1,3 @@
+module javike {
+	//hola
+}

6bdf44914eee425b8c6d4122d316fd646753fcba
alumno <alumno@alumno-1dam>
2021-06-01 10:34:10 +0200

    Saving new changes in our new brache in Vehiculo Class

--- javike/src/javike/Vehiculo.java
+++ javike/src/javike/Vehiculo.java
@@ -1,14 +1,10 @@
 package javike;
 
 public class Vehiculo {
-	private int numPuertas;
-	private int numRuedas;
-	
-	public Vehiculo ( int numPuertas, int numRuedas){
-		this. numPuertas=numPuertas;
-		this. numRuedas=numRuedas;}
-	public void showinfo(){
-		System.out.println( "Soy un vehiculo");
-				
+	int numRuedas = 4;
+	int numPuertas = 4;
+	boolean llevoRadio = true;
+	public Vehiculo() {
+		
 	}
 }

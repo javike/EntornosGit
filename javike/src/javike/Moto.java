e311c353c417c90cb2a4d5dbe6ea676e41939567
alumno <alumno@alumno-1dam>
2021-06-01 09:41:22 +0200

    New class moto created and methode showinfo.

---
+++ javike/src/javike/Moto.java
@@ -0,0 +1,11 @@
+package javike;
+
+public class Moto extends Vehiculo{
+
+	private boolean llevoRadio;
+	public Moto(int numPuertas, int numRuedas) {
+		super(0,2);
+		llevoRadio = false;
+	}
+
+}
--- javike/src/javike/Vehiculo.java
+++ javike/src/javike/Vehiculo.java
@@ -1,7 +1,15 @@
 package javike;
 
-public class Vehiculo (int numPuertas, int numRuedas){
- this.numPuertas = numPuertas;
- this.numRuedas = numRuedas;
-}
+public class Vehiculo {
+	private int numPuertas;
+	private int numRuedas;
+	
+	public Vehiculo ( int numPuertas, int numRuedas){
+		this. numPuertas=numPuertas;
+		this. numRuedas=numRuedas;}
+	public void showinfo(){
+		System.out.println( "Soy un vehiculo");
+				
+	}
+
 }
